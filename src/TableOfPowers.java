5875e5a44bbb1a7e38f39604dba4a31c3584c85e
IrishPebbles <lena.l.hand@gmail.com>
2017-12-20 16:36:03 -0500

    reverting change adding Math Class

--- src/TableOfPowers.java
+++ src/TableOfPowers.java
@@ -5,30 +5,26 @@ public class TableOfPowers {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 
-		int userNum = 0;
-		int userSquare = 0;
-		int userCube = 0;
-		char userChoice = 'y';
+//		int userNum = 0;
+		String userChoice = "y";
 		
 		System.out.println("Learn your squares and cubes!");
 		System.out.println();
 
-		while (userChoice == 'y' || userChoice == 'Y') {
+		while (userChoice.equalsIgnoreCase("y")) {
 			System.out.print("Enter an integer: ");
-			userNum = scnr.nextInt();
+//			userNum = scnr.nextInt();
 			System.out.println();
-
+			
 			System.out.printf("%-10s %-10s %-10s", "Number", "Squared", "Cubed");
 			System.out.println();
 
 			System.out.printf("%-10s %-10s %-10s", "========", "========", "========");
 			System.out.println();
 
-			for (int i = 1; i <= userNum; i++) {
-
-				userSquare = i * i;
-				userCube = i * i * i;
-				System.out.printf("%-10s %-10s %-10s", i, userSquare, userCube);
+			for (int i = 1; i <= scnr.nextInt(); i++) {
+				
+				System.out.printf("%-10s%-10s %-10s", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
 				System.out.println();
 
 			}
@@ -37,8 +33,10 @@ public class TableOfPowers {
 
 			do {
 				System.out.print("Would you like to continue? (y/n) ");
-				userChoice = scnr.next().charAt(0);
-			} while (userChoice != 'n' && userChoice != 'N' && userChoice != 'y' && userChoice != 'Y');
+				//userChoice = scnr.next().charAt(0);
+//				userChoice = scnr.nextLine().substring(0);
+				
+			} while (!scnr.nextLine().substring(0).equalsIgnoreCase("y") && !scnr.nextLine().substring(0).equalsIgnoreCase("n"));
 
 			System.out.println();
 		}
