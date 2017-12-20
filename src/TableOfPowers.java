7c0a25185195999816c9dcbf02cf2bf721cb3ab4
IrishPebbles <lena.l.hand@gmail.com>
2017-12-20 16:10:31 -0500

    Revert "Hopefully I didn't fuck this one up"
    This reverts commit daf4f2173fa771f7c03c70fff54c27f29dfb6c5c.

--- src/TableOfPowers.java
+++ src/TableOfPowers.java
@@ -5,26 +5,31 @@ public class TableOfPowers {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 
-//		int userNum = 0;
-		String userChoice = "y";
+		int userNum = 0;
+		int userSquare = 0;
+		int userCube = 0;
+		char userChoice = 'y';
 		
 		System.out.println("Learn your squares and cubes!");
 		System.out.println();
 
-		while (userChoice.equalsIgnoreCase("y")) {
+		while (userChoice == 'y' || userChoice == 'Y') {
 			System.out.print("Enter an integer: ");
-//			userNum = scnr.nextInt();
+			userNum = scnr.nextInt();
 			System.out.println();
-			
+
 			System.out.printf("%-10s %-10s %-10s", "Number", "Squared", "Cubed");
 			System.out.println();
 
 			System.out.printf("%-10s %-10s %-10s", "========", "========", "========");
 			System.out.println();
 
-			for (int i = 1; i <= scnr.nextInt(); i++) {
+			for (int i = 1; i <= userNum; i++) {
+
+				userSquare = (int) Math.pow(i, 2);
+				userCube = (int) Math.pow(i, 3);
 				
-				System.out.printf("%-10s%-10s %-10s", i, (int) Math.pow(i, 2), (int) Math.pow(i, 3));
+				System.out.printf("%-10s%-10s %-10s", i, userSquare, userCube);
 				System.out.println();
 
 			}
@@ -33,10 +38,8 @@ public class TableOfPowers {
 
 			do {
 				System.out.print("Would you like to continue? (y/n) ");
-				//userChoice = scnr.next().charAt(0);
-//				userChoice = scnr.nextLine().substring(0);
-				
-			} while (!scnr.nextLine().substring(0).equalsIgnoreCase("y") && !scnr.nextLine().substring(0).equalsIgnoreCase("n"));
+				userChoice = scnr.next().charAt(0);
+			} while (userChoice != 'n' && userChoice != 'N' && userChoice != 'y' && userChoice != 'Y');
 
 			System.out.println();
 		}
