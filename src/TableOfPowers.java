f722fa3b816bf0b4b8b2845f62262eb3eb808812
IrishPebbles <lena.l.hand@gmail.com>
2017-12-20 16:13:21 -0500

    Revert "added Math class"
    This reverts commit b789bf3948568e76384d3e136d15fc2e97c3bc86.

--- src/TableOfPowers.java
+++ src/TableOfPowers.java
@@ -26,10 +26,9 @@ public class TableOfPowers {
 
 			for (int i = 1; i <= userNum; i++) {
 
-				userSquare = (int) Math.pow(i, 2);
-				userCube = (int) Math.pow(i, 3);
-				
-				System.out.printf("%-10s%-10s %-10s", i, userSquare, userCube);
+				userSquare = i * i;
+				userCube = i * i * i;
+				System.out.printf("%-10s %-10s %-10s", i, userSquare, userCube);
 				System.out.println();
 
 			}
